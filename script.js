4bc72ad761050994f4f88b7fd62677635b432933
George Ngunjiri <ngunjirigeorge995@gmail.com>
2026-05-27 14:55:44 +0300

    feat(script.js):script eventlisteners for local storage

--- script.js
+++ script.js
@@ -8,3 +8,43 @@ const errorMsg = document.getElementById('errorMsg');
 const forecastContainer = document.getElementById('foreccast');
 
 // Load last searched city from localStorage
+window.addEventListener('load', () => {
+    const lastCity = localStorage.getItem('lastCity');
+    if (lastCity) {
+        cityInput.value = lastCity;
+        getWeather(lastCity);
+    }
+});
+
+searchBtn.addEventListener('click', () => {
+    const city = cityInput.value.trim();
+    if (city) getWeather(city);
+});
+
+cityInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        const city = cityInput.value.trim();
+        if (city) getWeather(city);
+    }
+});
+
+async function getWeather(city) {
+    try {
+        hideError ();
+        const response = await fetch(
+            `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`
+        );
+
+        if (!response.ok) throw new Error('City not found');
+
+        const data = await response.json();
+        displayWeather(data);
+        getWeather(city);
+        localStorage.setItem('lastCity', city);
+
+    } catch (error) {
+        showError('City not found. PLease try again.');
+        weatherCard.classList.add('hidden');
+        forecastContainer.innerHTML = '';
+    }
+}
